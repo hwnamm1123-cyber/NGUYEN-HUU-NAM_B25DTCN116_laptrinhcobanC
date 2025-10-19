730ca9b88106ddc35d848887a23792cfe538a6b3
hwnamm1123-cyber <hwnamm1123@gmail.com>
2025-10-19 16:39:10 +0700

    Add files via upload

--- Session 2/Bai8.cpp
+++ Session 2/Bai8.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int number = 12345, number1, number2;
-	int result = 0 ; 
-	number1 = number % 10;
-	number2 = number / 10;
-	result = result * 10 + number1;
+	int number = 12345, a, b;
+    int result = 0;
+    a = number % 10;
+    b = number / 10;
+    result = result * 10 + a;
+    a = b % 10;
+    b = b / 10;
+    result = result * 10 + a;
+    a = b % 10;
+    b = b / 10;
+    result = result * 10 + a;
+    a = b % 10;
+    b = b / 10;
+    result = result * 10 + a;
+    a = b % 10;
+    b = b / 10;
+    result = result * 10 + a;
 	printf("\nSo da duoc dao nguoc (result): %d\n", result);
     
     return 0; 
