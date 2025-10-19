839e55d5e7e58ce61337ad6086b148399b4ed80f
hwnamm1123-cyber <hwnamm1123@gmail.com>
2025-10-19 16:50:02 +0700

    Add files via upload

--- Session 2/Bai7.cpp
+++ Session 2/Bai7.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int number = 12345, number1, number2, Sum ;
-	number1 = number % 2;
-	number2 = number / 2;
-	Sum = number1 + number2;
+	int number = 12345, Sum;
+	Sum += number % 10;
+	number = number / 10;
+	Sum += number % 10;
+	number = number / 10;
+	Sum += number % 10;
+	number = number / 10;
+	Sum += number % 10;
+	number = number / 10;
+	Sum += number % 10;
 	printf ("Tong cua hai so la: %d", Sum);
 	return 0;
 	 
